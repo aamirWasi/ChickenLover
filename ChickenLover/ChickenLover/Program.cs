f0181e5d7dda33e7db560cf75c4153979edc1b97
aamir <wasi3aamir@gmail.com>
2019-01-21 14:39:07 +0600

    Accepted

---
+++ ChickenLover/ChickenLover/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenLover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int i=0; i<T; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(num >= 120)
+                {
+                    Console.WriteLine("Good Boy Sifat");
+                } else
+                {
+                    Console.WriteLine("Naughty Boy Sifat");
+                }
+            }
+
+        }
+    }
+}
